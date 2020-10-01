e1ae09224db85ea3d138e8e0c15025ac3a7d6a7d
D·Teach <1114626450@qq.com>
2020-10-01 22:51:36 +0800

    Spring组件注册

--- spring-annotation/src/main/java/com/dlq/tx/MyTxConfig.java
+++ spring-annotation/src/main/java/com/dlq/tx/MyTxConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * 声明式事务：
@@ -21,7 +24,36 @@ import org.springframework.jdbc.core.JdbcTemplate;
  * 5、配置事务管理器来控制事务;
  * 		@Bean
  * 		public PlatformTransactionManager transactionManager()
+ *
+ * 原理：
+ * 1）、@EnableTransactionManagement
+ * 			利用TransactionManagementConfigurationSelector给容器中会导入组件
+ * 			导入两个组件
+ * 			AutoProxyRegistrar
+ * 			ProxyTransactionManagementConfiguration
+ * 2）、AutoProxyRegistrar：
+ * 			给容器中注册一个 InfrastructureAdvisorAutoProxyCreator 组件；
+ * 			InfrastructureAdvisorAutoProxyCreator：？
+ * 			利用后置处理器机制在对象创建以后，包装对象，返回一个代理对象（增强器），代理对象执行方法利用拦截器链进行调用；
+ *
+ * 3）、ProxyTransactionManagementConfiguration 做了什么？
+ * 			1、给容器中注册事务增强器；
+ * 				1）、事务增强器要用事务注解的信息，AnnotationTransactionAttributeSource解析事务注解
+ * 				2）、事务拦截器：
+ * 					TransactionInterceptor；保存了事务属性信息，事务管理器；
+ * 					他是一个 MethodInterceptor；
+ * 					在目标方法执行的时候；
+ * 						执行拦截器链；
+ * 						事务拦截器：
+ * 							1）、先获取事务相关的属性
+ * 							2）、再获取PlatformTransactionManager，如果事先没有添加指定任何transactionmanger
+ * 								最终会从容器中按照类型获取一个PlatformTransactionManager；
+ * 							3）、执行目标方法
+ * 								如果异常，获取到事务管理器，利用事务管理回滚操作；
+ * 								如果正常，利用事务管理器，提交事务
+ *
  */
+@EnableTransactionManagement
 @ComponentScan("com.dlq.tx")
 @PropertySource("classpath:/db.properties")
 @Configuration
@@ -51,5 +83,12 @@ public class MyTxConfig {
     public JdbcTemplate jdbcTemplate(){
         //Spring对@Configuration类会特殊处理；给容器中加组件的方法，多次调用都只是从容器中找组件
         return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public DataSourceTransactionManager getDataSourceTransactionManager(DruidDataSource dataSource){
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        return transactionManager;
     }
 }
